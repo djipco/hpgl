360998bbc2337b574725a00222aedb7cd3814681
= <jp@cote.cc>
2016-10-24 21:49:54 -0400

    Updated documentation.

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Plotter"
+    ],
+    "modules": [
+        "hpgl"
+    ],
+    "allModules": [
+        {
+            "displayName": "hpgl",
+            "name": "hpgl",
+            "description": "The `hpgl` library makes it possible to interact with plotters and printers that support the\n*Hewlett-Packard Graphics Language* (a.k.a. *hpgl*). This language is the de facto standard for\nmost plotters."
+        }
+    ],
+    "elements": []
+} };
+});
