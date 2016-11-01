882f2a1de7715fb07c28050a4e444b4129623059
= <jp@cote.cc>
2016-10-31 21:24:53 -0400

    Got rid of uglify which does not work with ES6.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,19 +16,30 @@ module.exports = function (grunt) {
       files: ['package.json']
     },
 
-    uglify: {
+    // Uglify DOES NOT support ES6 !!!
+    // uglify: {
+    //   options: {
+    //     banner: "/*\n\n" + grunt.file.read('BANNER') + "\n\n" + grunt.file.read('LICENSE.txt') + "*/\n\n",
+    //     compress: {
+    //       drop_console: true
+    //     },
+    //     mangle: false,
+    //     preserveComments: false
+    //   },
+    //   build: {
+    //     src: './src/<%= pkg.name %>.js',
+    //     dest: './<%= pkg.name %>.min.js'
+    //   }
+    // },
+
+    concat: {
       options: {
         banner: "/*\n\n" + grunt.file.read('BANNER') + "\n\n" + grunt.file.read('LICENSE.txt') + "*/\n\n",
-        compress: {
-          drop_console: true
-        },
-        mangle: false,
-        preserveComments: false
       },
-      build: {
-        src: './src/<%= pkg.name %>.js',
-        dest: './<%= pkg.name %>.min.js'
-      }
+      dist: {
+        src: ['./src/<%= pkg.name %>.js'],
+        dest: './<%= pkg.name %>.js',
+      },
     },
 
     // // Generate doc
@@ -54,7 +65,7 @@ module.exports = function (grunt) {
           noStatus: false
         },
         files: {
-          src: ['<%= pkg.name %>.min.js', 'docs']
+          src: ['<%= pkg.name %>.js', 'docs']
         }
       }
     },
@@ -67,27 +78,27 @@ module.exports = function (grunt) {
       src: ['**/*']
     },
 
-    // release: {
-    //   options: {
-    //     bump: false,
-    //     commitMessage: 'Release <%= version %>'
-    //   }
-    // }
+    release: {
+      options: {
+        bump: false,
+        commitMessage: 'Release <%= version %>'
+      }
+    }
 
   });
 
   grunt.loadNpmTasks('grunt-bumpup');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   // grunt.loadNpmTasks('grunt-contrib-clean');
   // grunt.loadNpmTasks('grunt-contrib-compress');
   // grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks("grunt-contrib-uglify");
   grunt.loadNpmTasks('grunt-gh-pages');
   grunt.loadNpmTasks('grunt-git');
   grunt.loadNpmTasks('grunt-release');
   grunt.loadNpmTasks('grunt-jsdoc');
 
   grunt.registerTask('publish', ['publish:prerelease']);
-  grunt.registerTask("publish:prerelease", ['bumpup:prerelease', 'uglify', 'jsdoc', 'gitcommit:commitupdated', 'gh-pages', 'release']);
+  grunt.registerTask("publish:prerelease", ['bumpup:prerelease', 'concat', 'jsdoc', 'gitcommit:commitupdated', 'gh-pages', 'release']);
   grunt.registerTask("publish:patch", ['bumpup:patch', 'yuidoc', 'release']);
   grunt.registerTask('publish:minor', ['bumpup:minor', 'yuidoc', 'release']);
   grunt.registerTask('publish:major', ['bumpup:major', 'yuidoc', 'release']);
